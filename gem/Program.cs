62bc1a0801a1e683e143cc45459ff914fb56eaa4
codewitch-honey-crisis <gaztoast@gmail.com>
2024-08-08 18:38:55 -0700

    Add project files.

---
+++ gem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+using Json;
+partial class Program
+{
+	[CmdArg(Ordinal = 0, Required = true, Description = "The question to ask (use quotes)", ItemName = "question")]
+	static string Question;
+	[CmdArg(Name = "key", Description = "The API Key", ItemName = "key",Required =true)]
+	static string Key = "AIzaSyCse0M8u7n0Knf-VXMUagFplVP9k5a1mpQ";
+	const string _url_fmt = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={0}";
+	const string _json_prologue = "{\"contents\":[{\"parts\":[{\"text\":\""; const string _json_epilogue = "\"}]}]}";
+	static async Task MainAsync()
+	{
+		var client = new HttpClient();
+		//client.BaseAddress = ;
+		var content = new StringContent(_json_prologue + JsonUtility.EscapeString(Question) + _json_epilogue);
+		var result = await client.PostAsync(new Uri(string.Format(_url_fmt, Key)), content);
+		string resultContent = await result.Content.ReadAsStringAsync();
+		dynamic obj = JsonObject.Parse(resultContent);
+		Console.WriteLine(obj.candidates[0].content.parts[0].text);
+	}
+	static void Run()
+	{
+		MainAsync().Wait();
+	}
+}
+
